89b672eff0daf62e7e1671edd3f4f902b910c030
PRATIK GARAD <pratikgarad1221@gmail.com>
2026-03-28 14:45:54 +0530

    28_3_26_v1

--- script.js
+++ script.js
@@ -68,3 +68,281 @@ if(true){
 
 console.log(a);
 // console.log(b);
+
+// // let , var , const 
+// // declaration and initilization
+
+
+// // var a = 12 ;
+// // var a = 50;   
+// //  window me add karta hai 
+// // function scoped hota hai 
+// // redeclare kar shakte hai but error nahi aayega 
+
+// function abc(){
+//     if(true) {
+//         var a =22;
+//     }else{
+//         console.log(a)// The a is not accesible in the else beaucause of the limited 
+//                       // scope with the var>>
+//     }
+// }
+ 
+// // let a = 12 ;
+// // let a = 50;   //  In the let the redeclaration is not allowed
+
+
+// const b = 123;  // In the const value is not change 
+
+
+
+//  Topic---2
+
+// Scope (global, block, functional)
+// 1. var:(funxtionscoped)
+// var a =12; // this is used at overall the code
+// {
+//     var b =54; // this is also used overall code baeacuse the function is not craete
+              
+// }
+
+// function abc(){
+//     var c= 545;
+//     // this c variable scope is limited for the this function this is known as the block scope
+
+// }
+
+// 2.Let (blockscoped)
+
+
+// {
+//     let a = 12;// the let is acces only the blocked 
+// }
+
+
+
+// Topic 3 Initilizes and Redeclared
+
+// let a =65
+// a=69  // this is redeclared value
+
+
+// console.log(a)
+
+// var a =55;
+
+
+if(true){
+    var a =55;
+    let b = 56;
+}
+
+console.log(a);
+// console.log(b);// // let , var , const 
+// // declaration and initilization
+
+
+// // var a = 12 ;
+// // var a = 50;   
+// //  window me add karta hai 
+// // function scoped hota hai 
+// // redeclare kar shakte hai but error nahi aayega 
+
+// function abc(){
+//     if(true) {
+//         var a =22;
+//     }else{
+//         console.log(a)// The a is not accesible in the else beaucause of the limited 
+//                       // scope with the var>>
+//     }
+// }
+ 
+// // let a = 12 ;
+// // let a = 50;   //  In the let the redeclaration is not allowed
+
+
+// const b = 123;  // In the const value is not change 
+
+
+
+//  Topic---2
+
+// Scope (global, block, functional)
+// 1. var:(funxtionscoped)
+// var a =12; // this is used at overall the code
+// {
+//     var b =54; // this is also used overall code baeacuse the function is not craete
+              
+// }
+
+// function abc(){
+//     var c= 545;
+//     // this c variable scope is limited for the this function this is known as the block scope
+
+// }
+
+// 2.Let (blockscoped)
+
+
+// {
+//     let a = 12;// the let is acces only the blocked 
+// }
+
+
+
+// Topic 3 Initilizes and Redeclared
+
+// let a =65
+// a=69  // this is redeclared value
+
+
+// console.log(a)
+
+// var a =55;
+
+
+if(true){
+    var a =55;
+    let b = 56;
+}
+
+console.log(a);
+// console.log(b);// // let , var , const 
+// // declaration and initilization
+
+
+// // var a = 12 ;
+// // var a = 50;   
+// //  window me add karta hai 
+// // function scoped hota hai 
+// // redeclare kar shakte hai but error nahi aayega 
+
+// function abc(){
+//     if(true) {
+//         var a =22;
+//     }else{
+//         console.log(a)// The a is not accesible in the else beaucause of the limited 
+//                       // scope with the var>>
+//     }
+// }
+ 
+// // let a = 12 ;
+// // let a = 50;   //  In the let the redeclaration is not allowed
+
+
+// const b = 123;  // In the const value is not change 
+
+
+
+//  Topic---2
+
+// Scope (global, block, functional)
+// 1. var:(funxtionscoped)
+// var a =12; // this is used at overall the code
+// {
+//     var b =54; // this is also used overall code baeacuse the function is not craete
+              
+// }
+
+// function abc(){
+//     var c= 545;
+//     // this c variable scope is limited for the this function this is known as the block scope
+
+// }
+
+// 2.Let (blockscoped)
+
+
+// {
+//     let a = 12;// the let is acces only the blocked 
+// }
+
+
+
+// Topic 3 Initilizes and Redeclared
+
+// let a =65
+// a=69  // this is redeclared value
+
+
+// console.log(a)
+
+// var a =55;
+
+
+if(true){
+    var a =55;
+    let b = 56;
+}
+
+console.log(a);
+// console.log(b);// // let , var , const 
+// // declaration and initilization
+
+
+// // var a = 12 ;
+// // var a = 50;   
+// //  window me add karta hai 
+// // function scoped hota hai 
+// // redeclare kar shakte hai but error nahi aayega 
+
+// function abc(){
+//     if(true) {
+//         var a =22;
+//     }else{
+//         console.log(a)// The a is not accesible in the else beaucause of the limited 
+//                       // scope with the var>>
+//     }
+// }
+ 
+// // let a = 12 ;
+// // let a = 50;   //  In the let the redeclaration is not allowed
+
+
+// const b = 123;  // In the const value is not change 
+
+
+
+//  Topic---2
+
+// Scope (global, block, functional)
+// 1. var:(funxtionscoped)
+// var a =12; // this is used at overall the code
+// {
+//     var b =54; // this is also used overall code baeacuse the function is not craete
+              
+// }
+
+// function abc(){
+//     var c= 545;
+//     // this c variable scope is limited for the this function this is known as the block scope
+
+// }
+
+// 2.Let (blockscoped)
+
+
+// {
+//     let a = 12;// the let is acces only the blocked 
+// }
+
+
+
+// Topic 3 Initilizes and Redeclared
+
+// let a =65
+// a=69  // this is redeclared value
+
+
+// console.log(a)
+
+// var a =55;
+
+
+if(true){
+    var a =55;
+    let b = 56;
+}
+
+console.log(a);
+// console.log(b);
