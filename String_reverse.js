31ef6bed8eda76abd11d3cd111ac4a62d8faa5c0
PRATIK GARAD <pratikgarad1221@gmail.com>
2026-03-29 00:37:40 +0530

    29-3-26

---
+++ String_reverse.js
@@ -0,0 +1,23 @@
+let str="Hello Pratik";
+
+console.log(str);
+
+// function Reverse_Str(){
+//     return str.split('').reverse().join('');
+// }
+// console.log(Reverse_Str(str));
+
+
+// function Reverse_Str(str){
+//     let E_str='';
+
+//     for (let i=str.length-1; i>=0; i--){
+//         E_str+=str[i];
+//     }
+//     return E_str;
+// }
+
+// console.log(Reverse_Str(str));
+
+
+function 
