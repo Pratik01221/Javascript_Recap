37c6a4c1c026e148ab2b40e820e12db0c6b04f3f
PRATIK GARAD <pratikgarad1221@gmail.com>
2026-04-22 22:35:02 +0530

    22_4_26

--- DSA/reverseString.js
+++ DSA/reverseString.js
@@ -25,8 +25,62 @@ function ispalindrome(str){
 //     return largets;
 // }
 
-console.log(Findlarget([99,66,78,6]));
+// console.log(Findlarget([99,66,78,6]));
 
 function Findlarget(arr){
     return Math.max(...arr)
 }
+
+// let arr = [5,4,8,5];
+// arr = arr.slice(1);
+// console.log(arr);
+
+function greet (name,callback){
+    callback (`Hello, ${name}!`);
+}
+
+// greet("pratik", (message) => {
+//     console.log(message);
+// });
+
+// const add = (a , b) => a + b;
+
+// console.log(add(5,5));
+
+
+// const obj= {
+//     "name":"pratik"
+// }
+
+// obj.age=22;
+// console.log(obj);
+// delete obj.age;
+// console.log(obj);
+// console.log([1, 2, 3,5].reduce((a, b) => a + b));//adds numbers in the array, together using the reduce mehtod
+
+// console.log("fgh".repeat(3));
+
+function fibonachi(n){
+    let num1=0 , num2 = 1, nextnum;
+
+    console.log("Fibonacci Sequence");
+
+    for ( let i =1; i <=n; i++){
+        console.log(num1);
+        nextnum=num1+num2;
+        num1=num2;
+        num2=nextnum;
+    }
+}
+
+// fibonachi(7);
+
+function factorial (n){
+    let Ans=1;
+    for(let i =1; i<=n; i++){
+        Ans*=i;
+    }
+    return Ans;
+}
+
+// console.log(factorial(6));
