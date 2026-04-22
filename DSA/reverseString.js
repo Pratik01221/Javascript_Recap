c46a2d9e400f23fdce91515d1e36d9aa8c1fd230
PRATIK GARAD <pratikgarad1221@gmail.com>
2026-04-22 19:11:41 +0530

    22_4_26

--- DSA/reverseString.js
+++ DSA/reverseString.js
@@ -14,3 +14,19 @@ function ispalindrome(str){
 
 // console.log(ispalindrome("mam"));
 
+// function findLargest(arr){
+//     let largets=arr[0];
+//     for (let i=1; i<arr.length; i++){
+//         if (arr[i]>largets){
+//             largets=arr[i];
+//         }
+//     }
+
+//     return largets;
+// }
+
+console.log(Findlarget([99,66,78,6]));
+
+function Findlarget(arr){
+    return Math.max(...arr)
+}
