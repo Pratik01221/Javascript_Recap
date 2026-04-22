79ac1754afc96cc66779ec214989f893de013ec3
PRATIK GARAD <pratikgarad1221@gmail.com>
2026-04-22 19:01:08 +0530

    22_4_26

--- DSA/reverseString.js
+++ DSA/reverseString.js
@@ -4,5 +4,13 @@ function reverseString(str){
     return str.split("").reverse().join("");
 }
 
+// console.log(reverseString("hello world")); // Output: "dlrow olleh" 
 
-console.log(reverseString("hello world")); // Output: "dlrow olleh" 
+
+function ispalindrome(str){
+    const reversed = str.split("").reverse().join("");
+    return str === reversed;
+}
+
+// console.log(ispalindrome("mam"));
+
